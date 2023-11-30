bc4cc48d51749a52bcebbdec1b021df0013b494f
hrshdhgd <hrshdhgd@users.noreply.github.com>
2023-11-30 02:08:00 +0000

    Deploying to gh-pages from @ monarch-initiative/umls-ingest@bad08ff82b4554fa2298e27afb024824e43da90c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,4 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: '0.0.0.post8.dev0+465d412',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
